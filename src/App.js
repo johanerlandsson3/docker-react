8fedda0bc67bcbbe66b8e27e5ec823c2ed283409
johanerlandsson3 <johan_erland@hotmail.com>
2022-11-23 13:15:51 +0100

    Changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hejsan Snurre        </p>
+          Hejsan Vurre        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
