b7cdd98edf5b0865fe3ba906922f9ec58f10fb0d
johanerlandsson3 <johan_erland@hotmail.com>
2022-11-23 13:33:07 +0100

    changed in feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hejsan Vurre        </p>
+          Ändrad i feature branch        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
